c7cbf7268fba81cfdb72138bd589e226b1657059
Hubner Krisztian <hubner.krisztian97@gmail.com>
2017-08-26 17:33:51 +0200

    Contact section & maps edited

--- js/mapLoader.js
+++ js/mapLoader.js
@@ -1,8 +1,10 @@
 function myMap() {
+    var myCenter = new google.maps.LatLng(48.229130, 20.252578);
+    var marker = new google.maps.Marker({position:myCenter});
     var mapOptions = {
-        center: new google.maps.LatLng(48.23, 20.25),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
+        center: myCenter,
+        zoom: 17
     };
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    marker.setMap(map);
 }
