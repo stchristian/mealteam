37b4fd23c4d13b91763a9e9342dfd879091531d8
bencelaszlo <laszlo.bence.27@gmail.com>
2017-09-03 15:56:19 +0200

    Fix character encoding problem with new_post.php

--- admin/new_post.php
+++ admin/new_post.php
@@ -1,38 +1,44 @@
 <?php
-    include("config.php");
-    header("Content-type: text/html; charset=utf-8");
+mb_internal_encoding("UTF-8");
+header ('Content-type: text/html; charset=utf-8');
 
-    //input variables
-    $title;
-    $post;
-    $user;
+include("config.php");
 
-    //validiation
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["input-title"])) {
-            echo "Egy címet, légyszíves!";
+if (!mysqli_set_charset($connection, "utf8")) {
+    echo "Nem sikerült csatlakozni a MySQL adatbázishoz: " . mysqli_connect_error();
+}
+
+//input variables
+$title;
+$post;
+$user;
+
+//validiation
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["input-title"])) {
+        echo "Egy címet, légyszíves!";
+    } else {
+        $title = $_POST["input-title"];
+        if (empty($_POST["input-post"])) {
+            echo "Nem akarsz bejegyzést írni?";
         } else {
-            $title = $_POST["input-title"];
-            if (empty($_POST["input-post"])) {
-                echo "Nem akarsz bejegyzést írni?";
+            $post = $_POST["input-post"];
+            if (empty($_POST["input-user"])) {
+                echo "Csak van valami neved, nem?";
             } else {
-                $post = $_POST["input-post"];
-                if (empty($_POST["input-user"])) {
-                    echo "Csak van valami neved, nem?";
+                $user = $_POST["input-user"];
+                //insert new post into database
+                $sql = "INSERT INTO posts (title, post, user) VALUES('$title', '$post', '$user')";
+                if (mysqli_query($connection, $sql)) {
+                    echo "A bejegyzés közzététele sikeres. 🔫";
                 } else {
-                    $user = $_POST["input-user"];
-                    //insert into database
-                    $sql = "INSERT INTO posts (title, post, user) VALUES('$title', '$post', '$user')";
-                    if (mysqli_query($connection, $sql)) {
-                        echo "A bejegyzés közzététele sikeres. 🔫";
-                    } else {
-                        echo "Hiba: " . $sql . "<br>" . mysqli_error($connection);
-                    }
+                    echo "Hiba: " . $sql . "<br>" . mysqli_error($connection);
                 }
             }
         }
     }
+}
 
-    //close connection
-    mysqli_close($connection);
- ?>
+//close connection
+mysqli_close($connection);
+?>
